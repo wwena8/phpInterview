9b37c3bf64d86a45630fa55f85739228c9fce207
zhaoge <lizhao010@ke.com>
2020-05-30 12:22:53 +0800

    代码

--- ZUOSHEN/004.php
+++ ZUOSHEN/004.php
@@ -7,12 +7,41 @@
  */
 /**
  * 4 只利用递归函数实现栈的逆序
+ * 任何递归形式都可以转化为非递归形式
+ * 思路：
+ * 1 一个栈中得到栈底元素，并移除之
+ * 2 递归函数就是个栈
+ * @param SplStack $stack
+ * @return mixed
  */
-$p = new SplStack();
-$p->push(1);
-$p->push(2);
-$p->push(3);
-while (!$p->isEmpty()) {
-    $data = $p->pop();
-    echo $data;
+function getAndRemoveLast($stack)
+{
+    $result = $stack->pop();
+    if ($stack->isEmpty()) {
+        return $result;
+    } else {
+        $last = getAndRemoveLast($stack);
+        $stack->push($result);
+        return $last;
+    }
 }
+
+/**
+ * @param SplStack $stack
+ */
+function reverse($stack)
+{
+    if ($stack->isEmpty()) {
+        return;
+    }
+    $last = getAndRemoveLast($stack);
+    reverse($stack);
+    $stack->push($last);
+}
+
+$stack = new SplStack();
+$stack->push(1);
+$stack->push(2);
+$stack->push(3);
+reverse($stack);
+echo $stack->top();
